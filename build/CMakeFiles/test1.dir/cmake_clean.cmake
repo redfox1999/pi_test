64f3c68704b2e80087e9361e1be7794af44fd49a
redfox <8564093@qq.com>
2019-01-01 17:05:52 +0800

    LOG_WARN

---
+++ build/CMakeFiles/test1.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test1.dir/main.cc.o"
+  "CMakeFiles/test1.dir/simplog.c.o"
+  "test1.pdb"
+  "test1"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/test1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
